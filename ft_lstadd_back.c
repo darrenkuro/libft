b66333269692f6dbc225c35d332becc1865a051b
darrenkuro <darrenkuro@icloud.com>
2023-04-22 18:17:39 +0200

    style cleanup

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@42berlin.de>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 06:44:27 by dlu               #+#    #+#             */
-/*   Updated: 2023/04/22 18:05:04 by dlu              ###   ########.fr       */
+/*   Updated: 2023/04/22 18:17:04 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,10 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	int	len;
-
 	if (!lst)
 		return ;
-	len = ft_lstsize(*lst);
-	if (!len)
-	{
+	if (!*lst)
 		*lst = new;
-		return ;
-	}
-	while (--len > 0)
-		*lst = (*lst)->next;
-	(*lst)->next = new;
+	else
+		ft_lstlast(*lst)->next = new;
 }
