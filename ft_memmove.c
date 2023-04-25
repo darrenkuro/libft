10561369bc4f30dbcd6227b388e313c1c331c5ea
darrenkuro <darrenkuro@icloud.com>
2023-04-25 23:58:01 +0200

    fix overlap for memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@42berlin.de>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 13:31:19 by dlu               #+#    #+#             */
-/*   Updated: 2023/04/22 18:34:36 by dlu              ###   ########.fr       */
+/*   Updated: 2023/04/25 23:57:38 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	i = 0;
 	if (!dst && !src)
 		return (NULL);
-	while (++i <= len)
-		*(char *) dst++ = *(char *) src++;
-	return (dst - i + 1);
+	if (src < dst)
+		while (len--)
+			((char *) dst)[len] = ((char *) src)[len];
+	else
+		while (++i < len)
+			((char *) dst)[i - 1] = ((char *) src)[i - 1];
+	return (dst);
 }
