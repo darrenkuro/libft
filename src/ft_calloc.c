a5fbe8e4b8237382fc7d74eb7603fb4e14f406fa
darrenkuro <odon5ht@gmail.com>
2025-12-28 10:35:51 +0100

    Update ft_calloc

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,18 +6,17 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 11:12:34 by dlu               #+#    #+#             */
-/*   Updated: 2025/12/28 10:04:11 by dlu              ###   ########.fr       */
+/*   Updated: 2025/12/28 10:19:01 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <limits.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ret;
 
-	if (size && count && (size != 0 && count > SIZE_MAX / size))
+	if (size != 0 && count > SIZE_MAX / size)
 		return (NULL);
 	ret = malloc(count * size);
 	if (!ret)
